ccc1046410f38b6eb3f9e8d61ac0d3674b5bccb5
baconeggninja <lamontlachlan@gmail.com>
2017-07-01 07:21:24 +1200

    Initial Commit

---
+++ Fight for Flight/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  Fight for Flight
+//
+//  Created by Lachlan lamont on 1/07/17.
+//  Copyright (c) 2017 Lachlan Lamont. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    @IBOutlet weak var window: NSWindow!
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+    
+}
